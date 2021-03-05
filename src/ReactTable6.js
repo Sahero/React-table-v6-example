30260d71e47e41f8a7abf9b2afce22a1f089bb74
Sagar Shrestha <sagarshth@gmail.com>
2021-03-05 16:36:27 +1100

    Initial commit

---
+++ src/ReactTable6.js
@@ -0,0 +1,11 @@
+import ReactTable from "react-table-6";
+import "react-table-6/react-table.css";
+
+export default function ReactTable6() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
